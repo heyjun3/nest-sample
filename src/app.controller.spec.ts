9f4af6449c558d82fbb070469370293196303cdb
heyjun3 <t3k2g5kako@yahoo.co.jp>
2025-02-01 23:24:57 +0900

    learn jest

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -2,13 +2,23 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+// const mock = {
+//   getHello: jest.fn(() => 'Hello World!')
+// }
+const getHello = jest.fn(() => 'Hello World!')
+
 describe('AppController', () => {
   let appController: AppController;
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [AppService],
+      providers: [{
+        provide: AppService,
+        useValue: {
+          getHello,
+        },
+      }],
     }).compile();
 
     appController = app.get<AppController>(AppController);
@@ -18,5 +28,12 @@ describe('AppController', () => {
     it('should return "Hello World!"', () => {
       expect(appController.getHello()).toBe('Hello World!');
     });
+
+    it('mock app service', () => {
+      // jest.spyOn(mock, 'getHello').mockImplementation(() => "hello mock")
+      getHello.mockImplementationOnce(() => 'hello mock')
+
+      expect(appController.getHello()).toBe('hello mock')
+    })
   });
 });
